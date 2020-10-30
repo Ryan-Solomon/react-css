c9835a984542b31eae41d70b9f048ec7b24a1bbd
Ryan Solomon <ryanjsolomon24@gmail.com>
2020-10-29 19:43:46 -0500

    adds general layout

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,15 @@ import React from 'react';
 import './App.css';
 
 const App = () => {
-  return <div>Yo</div>;
+  return (
+    <div className='container'>
+      <header className='header'>Header Part</header>
+      <div className='content'>
+        <nav className='sidebar'>Nav</nav>
+        <main className='hotel-view'>Hotel View</main>
+      </div>
+    </div>
+  );
 };
 
 export default App;
